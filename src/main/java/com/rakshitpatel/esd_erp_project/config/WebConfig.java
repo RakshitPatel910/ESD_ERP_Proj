ab64b4c379c57b77279656a314dec47b16b3caa6
RakshitPatel910 <rakshitpatel910@gmail.com>
2024-11-23 16:28:14 +0530

    db and cors config

--- src/main/java/com/rakshitpatel/esd_erp_project/config/WebConfig.java
+++ src/main/java/com/rakshitpatel/esd_erp_project/config/WebConfig.java
@@ -4,16 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
 
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:5173")  // Frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-
 }
